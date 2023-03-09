89330dbd2afe21d91facf1b6b25dbe215d2b46d7
Sid Shingaki <sidalaytv@gmail.com>
2023-03-08 20:59:01 -0800

    initial commit

---
+++ .env.cmake
@@ -0,0 +1,7 @@
+
+set(GLFW_PATH C:/Dev/Libs/GLFW/3.3.8.WIN64)
+set(GLM_PATH C:/Dev/Libs/GLM/0.9.9.8)
+set(VULKAN_SDK_PATH  C:/Dev/Libs/VulkanSDK/1.3.239)
+
+# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
+set(MINGW_PATH "C:/Dev/Tools/msys64/mingw64")
